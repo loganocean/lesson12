756909a258bf168b9950442bd37e94e9b620ec5a
loganocean <43765068+loganocean@users.noreply.github.com>
2019-01-02 12:24:43 -0500

    Add files via upload

---
+++ lesson12/eightball.py
@@ -0,0 +1,25 @@
+import time
+import random
+
+print('-'*65)
+print('Magic Eight Ball')
+print()
+
+question = input('What is your question? ')
+print('Shaking!!!')
+time.sleep(0.5)
+print('...thinking...')
+time.sleep(0.5)
+print('...thinking...')
+time.sleep(0.5)
+
+choice = random.randint(1,4)
+
+if choice == 2:
+	print('Yes, do it!!!')
+elif choice == 2:
+	print('No!!!')
+elif choice == 3:
+	print('Maybe you should not ask a ball to think for you.')
+else:
+	print('Ask again...')
